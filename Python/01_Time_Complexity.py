4a6d48fe9ea0f26a2573c3e9c3d27915a0a1d6e9
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-09-16 14:28:20 +0530

    Learn how time complexity calculated.

---
+++ Python/01_Time_Complexity.py
@@ -0,0 +1,69 @@
+print("Data Structure & Algorithms in Python.") 
+
+print("Data means Information .") 
+print("Structure(Where store):( Array | LL | Stack | Queue | Tree | Graph ).") 
+print("Algorithums : Set of instructions. ") 
+
+print("--------------------------")
+print(" Comparision \n")
+print(" 1 < loglogn < logn < n < nSqr2 < nsqr3 < ...< 2powN \n ")
+
+print("--------------------------")
+print("Types of Analysis \n" )
+print("1.  Best Case :-  Omega Notation \n")
+print("2.  Average  Case :- Theta Notation \n")
+print("3.  Wrost Case :-  Big O- Notation \n")
+
+print("We always find wrost case time complexity of any program . \n")
+print(" omega(n) ")
+print(" Big O of (n sqr) ")
+
+print(" 1.) Constant Time: \n")
+print(" X= 5  ---> 1 unit")
+print(" print(x)  ---> 1 unit")
+print(" if(x==5): ")
+print(" print(Hello) ---> Constant ")
+print(" else: ")
+print(" print(Welcome) ----> Constant  \n")
+
+print("Big O of n = O(1)")
+
+print("\n")
+print("\n")
+print("\n")
+
+print("for i in range(0,n): ----> n times")
+print(" for j in range(0,m):  -----> m times")
+print("     for k in range(0,n): ------> n times")
+print("                 Welcome- : --->  n x m x n = n cube")
+print("Wrost time complexity: big O of (n cube)")
+
+print("\n")
+print("Most Dominating Factor: ")
+print("n = 5")
+print("if(n === 5):")
+print("     O(n sqr)")
+print("else:")
+print("     O(n cube)")
+
+print("\n Over all time complexity : big O of (n cube)")
+print("Most time taken considered.")
+
+print("\n")
+print("Logarithme Time")
+print(" n = 10")
+print(" while(n >= 1):")
+print(" print(Hello)")
+print(" n //2 \n")
+
+print("output like:")
+print("n=10   | n= 5   |     n=2    | n=1    | n=0 ")
+print("Hello  | Hello  |     Hello  | Hello  |  Hello ")
+
+print("Loop run k times so Order of (k) :  O(K)")
+print("n/2 , n/2 sqr , n/2 cube , .... so on .. = 1")
+print(" n = 2 pow K")
+print(" so we need log to solve ")
+print(" logn = log2 pow k")
+print(" k = logn / log 2")
+print(" so we can say = O(log n)")
