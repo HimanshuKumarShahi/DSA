ae4b78588882555ab1d13ccaf0cdaf5d9dc9ba1c
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-09-24 15:20:33 +0530

    Refactor removeDuplicates function and clean up commented code

--- Python/Array/01_Remove_Duplicates_from_Sorted_Array.py
+++ Python/Array/01_Remove_Duplicates_from_Sorted_Array.py
@@ -1,17 +1,32 @@
-from numpy import *
+# from numpy import *
 
 
-arr=array([1,2,3,1,2,2,3,3,4,5,6,6,6,6])
+# arr=array([1,2,3,1,2,2,3,3,4,5,6,6,6,6])
 
-i=0
-j=1
+# i=0
+# j=1
 
 # if not arr:
 #      return 0
 
-for j in range(len(arr)):
-    if(arr[i] != arr[j]):
-        i+1
-    arr[i] = arr[j]
+# for j in range(len(arr)):
+#     if(arr[i] != arr[j]):
+#         i+1
+#     arr[i] = arr[j]
 
 # return i + 1
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        i=0
+        for j in range(1,len(nums)):
+            if(nums[j] != nums[i]):
+                i+=1
+            nums[i]=nums[j]
+        return i+1
